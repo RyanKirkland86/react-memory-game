7dc54eab592a52bbd76560b8e1b5a9f817f5e690
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-02-22 18:30:59 -0800

    Adding Header component

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 // import CocktailCard from "./components/CocktailCard";
 import cocktails from "./cocktails.json";
 
 class App extends Component {
   // Setting this.state.cocktails to the cocktails json array
   state = {
-    cocktails: cocktails
+    cocktails: cocktails,
+    // score: score,
+    // highScore: highScore,
+    // clicked: []
   };
 
   render() {
     return (
-      <Navbar />
+      <div className="container-fluid">
+        <Navbar />
+        <Header />
+      </div>
     );
   }
 }
--- memorygame/src/components/Header/index.js
+++ memorygame/src/components/Header/index.js
@@ -3,8 +3,13 @@ import "./style.css";
 
 function Header(props) {
     return (
-        <div>
-            
+        <div className="row d-flex align-items-center w-100 p-3" id="header">
+            <div className="col-12">
+                <h1 className="text-center">Cocktail Memory Game!</h1>
+            </div>
+            <div className="col-12">
+                <p className="text-center">Click on an image to earn points, but don't click on it any more than once!</p>
+            </div>
         </div>
     )
 }
