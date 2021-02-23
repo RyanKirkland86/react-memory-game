ae3a49c0eea57cdf0272ed7990a84ab25a1116b4
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-02-22 17:23:41 -0800

    Adding components directories and files, importing them to root App.js file

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import CocktailCard from ".components/CocktailCard";
+import cocktails from "./cocktails.json";
 
-function App() {
-  return (
-
-  )
+class App extends Component {
+  // Setting this.state.cocktails to the cocktails json array
+  state = {
+    cocktails: cocktails
+  };
 }
 
 export default App;
